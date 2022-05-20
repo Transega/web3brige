ffc8bddbb07276f7ad2b2fd9f43d8711fefc0ffc
Transega <collinsasega.ca@gmail.com>
2022-05-20 11:23:27 +0300

    changed css and removed the default react app

--- src/App.js
+++ src/App.js
@@ -3,20 +3,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="Parking-app">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h3>Welcome to React </h3>
       </header>
     </div>
   );
